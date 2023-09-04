b9f9d106ecbd933d2d4c68665e23123fa4ddb856
EfrainRodriguez <efrainrg92@gmail.com>
2023-09-04 00:07:06 -0500

    adding secrets and vars

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <p>Deploy to Surge</p>
+      <p>Deploy to Surge - Adding secrets and vars</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
